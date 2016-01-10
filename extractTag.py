0938dd327ee646618545d18416969708b4fc9f2d
gaody <readyfei@gmail.com>
2016-01-10 11:53:50 +0800

    修复内容源判断

--- extractTag.py
+++ extractTag.py
@@ -41,6 +41,7 @@ def get_current_sync_state(note_store):
 
 
 def main():
+    global last_extrect_tag_time, last_extrect_tag_time
     token = json.load(open('./config/token.json','r'))
     if DEBUG:
         client = EvernoteClient(token=token['en_token'],sandbox=True)
@@ -59,6 +60,7 @@ def main():
     if os.path.exists(data_file('sync_state')):
         last_sync_state = json.load(open(data_file('sync_state'),'r'))
         last_update_count = last_sync_state['updateCount']
+        last_extrect_tag_time = last_sync_state['currentTime']
 
     #获取当前同步状态
     currnet_sync_state = get_current_sync_state(note_store)
@@ -83,19 +85,22 @@ def main():
         #print "guid: %s\tname:%s" %(tag.guid,tag.name)
         tag_name = tag.name.decode('utf-8').upper()
         my_tags[tag_name] = tag.guid
-    print '\t'.join(my_tags.keys())
+    print '  '.join(my_tags.keys())
 
     #操作新note
     for note in new_notes.notes:
+        #如果笔记创建时间小于上次同步时间
+        if note.created <= last_extrect_tag_time:
+            continue
         print '\n'+'*'*120
         content = note_store.getNoteContent(note.guid)
         print "guid:%s\ntitle:%s\ncreated:%s\n作者:%s" %(note.guid,note.title,note.created,note.attributes.author)
 
         print 'author:%s\nsource:%s\nsourceURL:%s\nsourceApplication:%s' %(note.attributes.author,note.attributes.source,note.attributes.sourceURL,note.attributes.sourceApplication)
 
-        if not note.attributes.sourceURL and not note.attributes.sourceApplication and not note.attributes.source:
+        if not note.attributes.sourceURL:
             continue
-        print "标签(tags):%s" %(",".join(note_store.getNoteTagNames(note.guid)))
+        print "现有标签(tags):%s" %(",".join(note_store.getNoteTagNames(note.guid)))
         print '-'*120
         #print "内容(%d):created:%s,\n%s" %(note.contentLength,note.created,content)
 
@@ -136,7 +141,7 @@ def main():
             if my_tags.get(keyword):
                 new_tag_guid.append(my_tags.get(keyword))
         newKeyWords = [item for item in set(my_tags.keys()) if item in set(keywords)]
-        print '\nbosonnlp与已有的tags交集:'
+        print '\nBosonNLP与已有的tags交集:'
         print '\t'.join(newKeyWords)
 
         #追加新笔记的tags
